144f0c24b9c903b04a2230dc4a964ff0c17cc456
Ahmed Ali Mostafa <119663082+MatMan04@users.noreply.github.com>
2025-02-11 15:36:27 +0200

    Create fundamentals.py

---
+++ fundamentals.py
@@ -0,0 +1,29 @@
+# print(type(100)) >>>  str
+# print(type(100.8)) >>> float
+# print(type([1,3,4,5]))>>> list 
+# print(type({1,3,4,5}))  >>> set 
+# print(type((1,3,4,5)))  >>> tuple 
+# print(type({"one":2,"two":1,"three":5}))   >>>>>dict
+# print(type(2==2)) >>>> bool
+# name= "osama"
+# print(name)
+# my_name="Ahmed"
+# print(my_name)
+# x=10
+# x="Hello"
+# print(x)    
+# help("keywordar)
+# my_areas=['ahmed',1,'ali',2,'reha',4]
+# my_areas.reverse()
+# my_areas.append("Mohamed")
+# print(my_areas)
+# import numpy as np
+# height=[40.55,1.68,1.71,1.89,1.97]
+# np_height= np.array(height)
+# weight=[65,54.4,80,90,55]
+# np_weight=np.array(weight)
+# aa=np_height>23
+# print(np_height[aa])
+# print(np.array([True,1,2])+np.array([3,4,False]) 
+# x= 5.00
+# print(type(x))
